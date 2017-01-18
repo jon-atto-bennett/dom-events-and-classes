aab38742cef4c855bd8c2703bcf4c9c23b67c185
jon-atto-bennett <jon.bennett@outlook.co.nz>
2017-01-19 12:33:01 +1300

    code for count

--- game.js
+++ game.js
@@ -45,7 +45,21 @@ function updateCounts () {
   }
 
   // WRITE CODE HERE TO COUNT BLUE, GREEN, AND INVISIBLE DOTS
+  var allDots = document.getElementsByClassName('board')[0].children
 
+  for (var i = 0; i < allDots.length; i++) {
+
+    if (allDots[i].classList.contains('blue')) {
+    totals.blue += 1;
+    }
+    else if (allDots[i].classList.contains('green')) {
+    totals.green += 1;
+    }
+    else if (allDots[i].classList.contains('invisible')) {
+    totals.invisible += 1;
+    }
+
+}
   // Once you've done the counting, this function will update the display
   displayTotals(totals)
 }
